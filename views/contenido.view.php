5c77ccd9b3e3264dacb1b8a946ff0092a78f8e11
CarlosHdz7 <carloshdz.udb@gmail.com>
2018-03-28 13:32:24 -0600

    Firts commit

---
+++ views/contenido.view.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"> 
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Contenido</title>
+</head>
+<body>
+    <div class="contenedor">
+        <h1 class="titulo">Contenido del sitio</h1>
+        <a href="cerrar.php">Cerrar sesion</a>
+        <hr class="border">
+        <div class="contenido">
+            <article>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur obcaecati commodi inventore illum, nostrum esse voluptas ex nobis nesciunt autem voluptates ut assumenda nisi. Distinctio inventore et consequuntur officia fuga?</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur obcaecati commodi inventore illum, nostrum esse voluptas ex nobis nesciunt autem voluptates ut assumenda nisi. Distinctio inventore et consequuntur officia fuga?</p>
+            </article>
+        </div>
+    </div>
+</body>
+</html>
